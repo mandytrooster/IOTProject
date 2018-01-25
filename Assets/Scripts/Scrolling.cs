ab80106a4a682828e38e8f3a68ef253f9b5ee66c
Mandy Trooster <mandytrooster@gmail.com>
2018-01-25 14:45:54 +0100

    test if scrolling works better now

--- Assets/Scripts/Scrolling.cs
+++ Assets/Scripts/Scrolling.cs
@@ -3,53 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO.Ports;
 
-public class Scrolling : MonoBehaviour {
+public class Scrolling : MonoBehaviour
+{
 
-	private Rigidbody2D rb;
-	private int speed = 4;
-	public static bool stopScrolling = false;
-	public int tempInt;
-//	public int scroll = 4; //sets initial scroll speed to 0
+    private Rigidbody2D rb;
+    private int speed = 4;
+    public static bool stopScrolling = false;
+    public int tempInt;
+    //	public int scroll = 4; //sets initial scroll speed to 0
+    string serialInput;
+    SerialPort serial = new SerialPort("/dev/cu.usbmodem1421", 9600);
 
-	SerialPort serial = new SerialPort("/dev/cu.usbmodem1421", 9600);
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(-speed, 0);
 
-	void Start ()
-	{
-		rb = GetComponent<Rigidbody2D> ();
-		rb.velocity = new Vector2 (-speed, 0);
+        if (!serial.IsOpen)
+        {
+            serial.Open();
+        }
+        serial.ReadTimeout = 1;
+        serial.DataReceived += DataReceivedHandler;
 
-		if (!serial.IsOpen) {
-			serial.Open ();
-		}
-		serial.ReadTimeout = 1;
+        // Parsed de waarde van seriele verbinding naar de integer speed. Lukt dit niet -> default gravity 
+        System.Int32.TryParse(serialInput, out speed);
+    }
 
-		// Parsed de waarde van seriele verbinding naar de integer speed. Lukt dit niet -> default gravity 
-		System.Int32.TryParse(serial.ReadLine(), out tempInt);
-		speed = tempInt;
+    private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+    {
+        SerialPort sp = (SerialPort)sender;
+        string serialInput = sp.ReadLine();
+        Debug.Log(serialInput);
+    }
 
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (stopScrolling)
+        {
+            rb.velocity = Vector2.zero;
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
 
-	// Update is called once per frame
-	void Update () 
-	{
-		if (stopScrolling) {
-			rb.velocity = Vector2.zero;
-		}  else {
-			rb.velocity = new Vector2 (-speed, 0);
-		} 
+        System.Int32.TryParse(serialInput, out speed);
+        //Debug.Log(speed);
 
+    }
 
-		 try {
-			System.Int32.TryParse(serial.ReadLine(), out tempInt);
-
-			// als tempInt niet gelijk is aan de huidige waarde van gravity -> verander de gravity naar de nieuwe waarde.
-			//if (!(speed == tempInt)) {
-				speed = tempInt;
-			//}
-		}
-		catch(System.TimeoutException) {} 
-		Debug.Log (speed); 
-
-	}
 }
+
+
+
 
